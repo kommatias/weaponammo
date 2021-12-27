c9e1cf8d2ab6e812751bb38c7ea7f80aaed302ee
kommatias <71037072+kommatias@users.noreply.github.com>
2021-12-28 01:57:04 +0200

    esx = nil

--- server.lua
+++ server.lua
@@ -1,3 +1,9 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj)
+	ESX = obj
+end)
+
 -- Weapon Box
 ESX.RegisterUsableItem('weabox', function(source, cb)
 	local xPlayer = ESX.GetPlayerFromId(source)
