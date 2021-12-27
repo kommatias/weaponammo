813cc9c9d97243948de04ae90801b4fdb97e1982
Musiker15 <info@musiker15.de>
2021-12-27 23:17:02 +0100

    Update v3.0

--- config.lua
+++ config.lua
@@ -4,17 +4,16 @@ Config.Locale = 'de'
 -- Set true if you're using this Inventory
 Config.ChezzaInventory = false -- https://chezza.tebex.io/package/4770357
 
--- Edit to your Liking | true = It will remove that item when used | false = Does opposite of true
+-- Set true if you like to remove item when used
 Config.Removeables = {
 	WeaponBox = true,
 	WeaponClip = true,
 }
 
--- Weapon Ammos
 Config.CheckMaxAmmo = true -- If 'false' the Player can get more than Config.MaxAmmo bullets
  	Config.MaxAmmo = 250 -- Set the max. Ammo in Weapon
-Config.WeaponBoxAmmo = 150 -- Will give this many buillets to player
-Config.WeaponClipAmmo = 30 -- Will give this many buillets to player
+Config.WeaponBoxAmmo = 150 -- Will give this many bullets to player
+Config.WeaponClipAmmo = 30 -- Will give this many bullets to player
 
 Config.Weapons = {
 	-- Pistols
